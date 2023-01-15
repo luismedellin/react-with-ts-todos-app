78a877d487e557730f2198c67058ab46a9d311da
Luis Velandia <v-lvelandia@youngliving.com>
2023-01-14 22:43:23 -0500

    Init project

---
+++ react+ts/todo-app/src/todo/Todo.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Todo = () => {
+  return (
+    <>
+        <h1>Todo: </h1>
+        <ul>
+            <li>TODO 1</li>
+            <li>TODO 2</li>
+            <li>TODO 3</li>
+        </ul>
+    </>
+  )
+}
