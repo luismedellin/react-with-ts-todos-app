8b3abfd81286218ec6e6d4ec43c34005fe8cfee1
Luis Velandia <v-lvelandia@youngliving.com>
2023-01-14 23:25:08 -0500

    Config context

--- react+ts/todo-app/src/todo/context/TodoProvider.tsx
+++ react+ts/todo-app/src/todo/context/TodoProvider.tsx
@@ -1,12 +1,38 @@
+import { useReducer } from "react";
+import { TodoState } from "../interfaces/interfaces";
 import { TodoContext } from "./TodoContext";
+import { todoReducer } from "./todoReducer";
+
+const INITIAL_STATE: TodoState = {
+    todoCount: 2,
+    todos: [
+        {
+            id: '1',
+            desc: 'Recolectar las piedras del infinito',
+            completed: false 
+        },
+        {
+            id: '2',
+            desc: 'Piedra del alma',
+            completed: false 
+        },
+    ],
+    completed: 0,
+    pending: 2
+}
 
 interface props {
     children: JSX.Element | JSX.Element[]
 }
 
 export const TodoProvider = ({children}: props) => {
+
+    const [ todoState, dispatch] = useReducer(todoReducer, INITIAL_STATE );
+
   return (
-    <TodoContext.Provider value={{}}>
+    <TodoContext.Provider value={{
+        todoState
+    }}>
         { children }
     </TodoContext.Provider>
   )
