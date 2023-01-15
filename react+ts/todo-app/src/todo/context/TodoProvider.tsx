4aa18840f7b02723c992481f8e36e39873650841
Luis Velandia <v-lvelandia@youngliving.com>
2023-01-14 22:51:59 -0500

    Add provider

---
+++ react+ts/todo-app/src/todo/context/TodoProvider.tsx
@@ -0,0 +1,13 @@
+import { TodoContext } from "./TodoContext";
+
+interface props {
+    children: JSX.Element | JSX.Element[]
+}
+
+export const TodoProvider = ({children}: props) => {
+  return (
+    <TodoContext.Provider value={{}}>
+        { children }
+    </TodoContext.Provider>
+  )
+}
